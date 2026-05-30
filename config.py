561e059584c9982f09d713bb97682f9c10cbd6bb
Ajay221B2 <140782896+Ajay221B2@users.noreply.github.com>
2026-05-30 15:49:43 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,18 @@
+WEBSITES = [
+    "https://thefintechtimes.com/category/trending/",
+    "https://bfsi.economictimes.indiatimes.com/tag/fintech?utm_source=main_menu_dropdown&utm_medium=latestNews",
+    "https://economictimes.indiatimes.com/markets",
+    "https://inc42.com/buzz/?utm_medium=referral&utm_source=menu" ,
+  
+]
+
+KEYWORDS = [
+    "indian stock market",
+    "startup funding india",
+    "RBI policy",
+    "nifty sensex",
+    # add your own
+]
+
+RECIPIENT_EMAIL = "you@gmail.com"
+DIGEST_SUBJECT  = "📰 Weekly Market Intelligence Digest"
